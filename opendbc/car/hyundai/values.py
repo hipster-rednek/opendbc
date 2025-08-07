e8cbb670dc3d0248d2b6044c295ce0c435f5d37a
magicbear <matt@uucp.ca>
2025-08-07 08:54:21 -0500

    Add Genesis GV80 2021 with HDA II support

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -605,6 +605,11 @@ class CAR(Platforms):
     CarSpecs(mass=2258, wheelbase=2.95, steerRatio=14.14),
     flags=HyundaiFlags.RADAR_SCC,
   )
+  GENESIS_GV80_2021 = HyundaiCanFDPlatformConfig(
+    [HyundaiCarDocs("Genesis GV80 (with HDA II) 2021", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q]))],
+    CarSpecs(mass=2258, wheelbase=2.95, steerRatio=14.14),
+    flags=HyundaiFlags.RADAR_SCC,
+  )
 
 
 class Buttons:
